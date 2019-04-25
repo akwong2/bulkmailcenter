75218473bf1e7fe43cd4a829f2f80e6aa9b9a4b3
Alvin <akwong2@uci.edu>
2019-04-24 21:11:52 -0700

    fix some media css for navbar

--- src/components/Navigation/NavItems/NavItems.js
+++ src/components/Navigation/NavItems/NavItems.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './NavItems.css';
 
 const navItems = (props) => {
     return (
-        <ul>
-            <li><NavLink to="/" exact>Home</NavLink></li>
-            <li><NavLink to="/mail">Mailing Services</NavLink></li>
-            <li><NavLink to="/data">Data Processing</NavLink></li>
-            <li><NavLink to="/print">Duplication/ Printing</NavLink></li>
-            <li><NavLink to="/procurement">Procurement</NavLink></li>
-            <li><NavLink to="/testimonial">Testimonial</NavLink></li>
-            <li><NavLink to="/contact">Contact Us</NavLink></li>
-            <li><NavLink to="/direction">Directions</NavLink></li>
-        </ul>
+        <div className="NavItems"> 
+            <ul>
+                <li><NavLink to="/" exact>Home</NavLink></li>
+                <li><NavLink to="/mail">Mailing Services</NavLink></li>
+                <li><NavLink to="/data">Data Processing</NavLink></li>
+                <li><NavLink to="/print">Duplication/ Printing</NavLink></li>
+                <li><NavLink to="/procurement">Procurement</NavLink></li>
+                <li><NavLink to="/testimonial">Testimonial</NavLink></li>
+                <li><NavLink to="/contact">Contact Us</NavLink></li>
+                <li><NavLink to="/direction">Directions</NavLink></li>
+            </ul>
+        </div>
     )
 };
 
