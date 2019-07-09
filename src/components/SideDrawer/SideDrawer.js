fa1d9d222cdab72c81e1f420f184c864310d4aa3
Alvin <akwong2@uci.edu>
2019-07-08 18:44:03 -0700

    make footer and sidefooter into clickable links

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -18,16 +18,20 @@ const sideDrawer = (props) => {
                 <NavItems />
                 <div className="SideFooter">
                     <div>
-                        {/* <div>Phone: (510) 835-8117</div>
-                        <div>Fax: (510) 835-1720</div>
-                        <div>info@bulkmailcenter.com</div>
-                        <div>777 W Grand Ave, Oakland, CA 94612</div> */}
-                        <div className="Phone">Phone: (510) 835-8117</div>
+                        <div className="Phone">Phone: 
+                            <a href="tel:510-835-8117"> (510) 835-8117</a>
+                        </div>
                         <div className="Fax">Fax: (510) 835-1720</div>
-                        <div className="Address">777 W Grand Ave, Oakland, CA 94612</div>
-                        <div className="Email">info@bulkmailcenter.com</div>
+                        <div className="Address">
+                            <a href="https://www.google.com/maps/dir//37.8129431,-122.2754464/@37.8128413,-122.2799521,16z?hl=en">
+                                777 W Grand Ave, Oakland, CA 94612
+                            </a>
+                        </div>
+                        {/* "https://www.google.com/maps/dir//37.8129431,-122.2754464/@37.8128413,-122.2799521,16z?hl=en" */}
+                        <div className="Email">
+                            <a href="mailto:info@bulkmailcenter.com">info@bulkmailcenter.com</a>
+                        </div>    
                     </div>
-                    
                 </div>
             </div>
         </Aux>
