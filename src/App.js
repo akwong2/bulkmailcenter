1c12ab5f0991b7309ffe8118f8efbd1fd64d9f71
Alvin <akwong2@uci.edu>
2019-07-11 22:18:48 -0700

    rename import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Layout from './containers/Layout/Layout';
+import Layout from './Containers/Layout/Layout.js';
 import './App.css';
 
 class App extends Component {
