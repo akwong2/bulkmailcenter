6e091e1751c19945608dcd879c4be1f4ba53c34e
Alvin <akwong2@uci.edu>
2019-07-11 18:07:21 -0700

    fix direction spacing

--- src/containers/Direction/Direction.js
+++ src/containers/Direction/Direction.js
@@ -46,40 +46,40 @@ class Direction extends Component {
 	}
 
   render() {
-	let apiKey = {key: this.state.api}
-	let show = <div></div>
-	if (this.state.api !== '') {
-		show = 
-		<div className="Direction" >
-			<GoogleMapReact
-				bootstrapURLKeys={apiKey}
-				defaultCenter={this.props.center}
-				defaultZoom={this.props.zoom}
-				onChildClick={this.childClick}
-				onChildMouseEnter={this.markerHover}
-				onChildMouseLeave={this.markerHover}
-			>
-				<Hover 
-					lat={37.8129431}
-					lng={-122.2754464}
-					hover={this.state.hover}
-					balloon={this.state.balloon}
-				/>
-			</GoogleMapReact>
-			<p>
-				<a href="https://www.google.com/maps/dir//37.8129431,-122.2754464/@37.8128413,-122.2799521,16z?hl=en">
-					777 W Grand Ave, Oakland, CA 94612
-				</a>
-			</p>
-		</div>
-	}
-    return (
-		// Important! Always set the container height explicitly
-		<div>
-			<h2>Directions</h2>
-			{show}
-      </div>
-    );
+		let apiKey = {key: this.state.api}
+		let show = <div></div>
+		if (this.state.api !== '') {
+			show = 
+			<div className="Direction" >
+				<GoogleMapReact
+					bootstrapURLKeys={apiKey}
+					defaultCenter={this.props.center}
+					defaultZoom={this.props.zoom}
+					onChildClick={this.childClick}
+					onChildMouseEnter={this.markerHover}
+					onChildMouseLeave={this.markerHover}
+				>
+					<Hover 
+						lat={37.8129431}
+						lng={-122.2754464}
+						hover={this.state.hover}
+						balloon={this.state.balloon}
+					/>
+				</GoogleMapReact>
+				<p>
+					<a href="https://www.google.com/maps/dir//37.8129431,-122.2754464/@37.8128413,-122.2799521,16z?hl=en">
+						777 W Grand Ave, Oakland, CA 94612
+					</a>
+				</p>
+			</div>
+		}
+		return (
+			// Important! Always set the container height explicitly
+			<div>
+				<h2>Directions</h2>
+				{show}
+				</div>
+		);
   }
 }
  
