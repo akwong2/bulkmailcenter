c671f415d95520db3460929a0aa2fec4fd7290f1
Alvin <akwong2@uci.edu>
2019-07-17 21:17:37 -0700

    reverse proxies

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 require('dotenv').config() 
 
 if (process.env.NODE_ENV === "production") {
